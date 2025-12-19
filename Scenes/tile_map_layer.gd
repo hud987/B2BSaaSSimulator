c229060f9c5337c973255f98c7cf10b50a485cc0
Michael Hudachek <hud987@gmail.com>
2025-12-18 20:15:54 -0500

    grid shader

--- Scenes/tile_map_layer.gd
+++ Scenes/tile_map_layer.gd
@@ -1,13 +1,24 @@
-extends TileMapLayer
-#extends ColorRect
+extends ColorRect
 
 @export var cell_size : Vector2 = Vector2(32, 32)
 @export var grid_size : Vector2i = Vector2i(40, 30)
 
-func _draw():
-	# Draw vertical lines
-	for x in grid_size.x + 1:
-		draw_line(Vector2(x * cell_size.x, 0), Vector2(x * cell_size.x, grid_size.y * cell_size.y), Color.DIM_GRAY, 1.0)
-	# Draw horizontal lines
-	for y in grid_size.y + 1:
-		draw_line(Vector2(0, y * cell_size.y), Vector2(grid_size.x * cell_size.x, y * cell_size.y), Color.DIM_GRAY, 1.0)
+@onready var shader : ShaderMaterial = material as ShaderMaterial
+
+func _on_mouse_entered() -> void:
+	print("mouse entered grid")
+
+func _gui_input(event) -> void:
+	if event is InputEventPanGesture and not is_zero_approx(event.delta.y):
+		handle_scroll(event.delta.y)
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_WHEEL_UP and event.pressed:
+			print("Wheel up")
+
+func handle_scroll(delta : float) -> void:
+	var local_mouse := get_local_mouse_position()
+	var uv := local_mouse / size
+	shader.set_shader_parameter("mouse_uv", uv)
+	
+	var zoom : float = shader.get_shader_parameter("zoom") + delta * 0.5
+	shader.set_shader_parameter("zoom", clamp(zoom, 1.0, 50.0))
