aca62a43dde4375f9fb2a069d220d2517f06e57e
unknown <hud987@gmail.com>
2026-01-26 17:09:05 -0500

    collapsable menu

--- Scenes/grid_container.gd
+++ Scenes/grid_container.gd
@@ -1,26 +1,34 @@
-extends GridContainer
+extends Control
 	
 @export var is_expanded : bool = true
 @export var time : float = 0.1
 @export var transition_type : Tween.TransitionType
+@onready var resourcesGridContainer : GridContainer = self.get_child(0)
 
+#@onready var button: Button = %CollapseTestButton
+#@onready var collapse_control: Control = %CollapseControl
+
+#Setting this node to "Clip Contents = True" is key
 #Force the initial render to be correct
 func _ready() -> void:
 	await get_tree().process_frame #Can force dropdown to render closed
-	print("custom minimum: ", self.custom_minimum_size.y)
-	print("is_expanded: ", is_expanded)
-	scale.y = 1.0 if is_expanded else 0.0
+	custom_minimum_size.y = resourcesGridContainer.size.y + 7
+	self.size.y = custom_minimum_size.y if is_expanded else 0.0
 
-func _on_button_pressed() -> void:
-	print("button pressed", self.scale)
-	self.expand()
 
 func expand() -> void:
+	print("custom_minimum_size.y: ", custom_minimum_size.y)
 	if is_expanded:
-		set_tween("scale", Vector2(1,0), time)
+		set_tween("custom_minimum_size:y", 0, time)
 	else:
-		set_tween("scale", Vector2(1,1), time)
+		set_tween("custom_minimum_size:y", resourcesGridContainer.size.y + 7, time)
 	is_expanded = !is_expanded
 
 func set_tween(property: String, val: Variant, duration: float) -> void:
-	get_tree().create_tween().tween_property(self, property, val, duration).set_trans(transition_type)
+	create_tween().tween_property(self, property, val, duration).set_trans(transition_type)
+
+func _on_compute_button_pressed() -> void:
+	self.expand()
+
+func _on_storage_button_pressed():
+	self.expand()
